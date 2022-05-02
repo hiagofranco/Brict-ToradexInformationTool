8dffe728bec1337b2654ba4e0590cdda97416a3c
hiagofranco <hiagofranco@gmail.com>
2022-05-02 17:14:58 -0300

    Updated info and added argument options

--- tdx_version.sh
+++ tdx_version.sh
@@ -9,18 +9,56 @@ if [ "`id -u`" != "0" ]; then
 	exit
 fi
 
-echo "Software info:"
-echo "-----------------"
-echo "kernel:[`uname -r`]"
-echo "kernel-version:[`uname -v`]"
-echo "kernel:[`uname -r`]"
-echo "-----------------"
-echo "os-release:\n`cat /etc/os-release`"
-echo "-----------------"
-echo "\nHardware info:"
-echo "-----------------"
-echo "processor:[`uname -m`]"
-echo "-----------------"
-echo "all devices:\n`ls -lh /dev`"
-echo "-----------------"
-echo "END"
+software_info ()
+{
+    echo "Software info:"
+    echo "-----------------"
+    echo "uname-all:[`uname -a`]"
+    echo "kernel:[`uname -r`]"
+    echo "kernel-version:[`uname -v`]"
+    echo "-----------------"
+    echo "/etc/os-release:\n`cat /etc/os-release`"
+    echo "-----------------"
+    echo "/etc/issue:`cat /etc/issue`"
+    echo "-----------------"
+    echo "uboot-version:"
+}
+
+hardware_info ()
+{
+    echo "\nHardware info:"
+    echo "-----------------"
+    echo "processor:[`uname -m`]"
+    echo "device-tree-overlays:[]"
+    echo "fdtfile:[]"
+    echo "fdt_board:[]"
+    echo "-----------------"
+}
+
+devices_info ()
+{
+    echo "\nAll devices:\n`ls -lh /dev`"
+    echo "-----------------"
+    echo "END"
+}
+
+case $1 in
+    "--software" | "-s")
+        software_info
+        ;;
+    "--hardware" | "-h")
+        hardware_info
+        ;;
+    "--devices" | "-d")
+        devices_info
+        ;;
+    "--no-devices" | "-nd")
+        software_info
+        hardware_info
+        ;;
+    *)
+        software_info
+        hardware_info
+        devices_info
+        ;;
+esac
