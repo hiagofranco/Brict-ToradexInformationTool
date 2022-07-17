261c5b6fc7dc9675b08b9c978daea461e5c985f2
Leonardo Graboski Veiga <leogveiga@gmail.com>
2022-07-16 23:41:42 -0300

    print: use formatting functions
    Standardize print with formatting functions. This makes it easier to
    contribute with new data points, as the contributor doesn't need to
    think about formatting.
    
    It also improves the UX for reading data, as it is tabulated and
    aligned. Of course, there is a limitation with the screen width of each
    user's computer.

--- tdx_version.sh
+++ tdx_version.sh
@@ -4,6 +4,45 @@
 # Date: may-02-2022
 # Author: hiagofranco & g-claudino
 
+HORIZONTAL_LINE_WIDTH=60
+TABULATION_WIDTH=25
+
+print_header ()
+{
+    if [ -n "$1" ]; then
+        echo ""
+        echo "$1"
+        printf "%0.s-" $(seq 1 $HORIZONTAL_LINE_WIDTH)
+        printf "\n"
+    else
+        echo 'Error: "print_header" called without a parameter!'
+        exit 1
+    fi
+}
+
+print_info ()
+{
+    if [ -z "$1" ] ; then
+        echo 'Error: "print_info" is missing parameter(s)!'
+        exit 1
+    else
+        if [ -z "$2" ]; then
+            printf "%-${TABULATION_WIDTH}s %s\n" "$1:" "-"
+        elif [ "$(echo "$2" | sed -re '/^$/d' | wc -l)" -gt 1 ]; then
+            echo "$1:"
+            echo "$2" | sed "s/^/ $(printf "%0.s " $(seq 1 $TABULATION_WIDTH))/"
+        else
+            printf "%-${TABULATION_WIDTH}s %s\n" "$1:" "$2"
+        fi
+    fi
+}
+
+print_footer ()
+{
+    printf "%0.s-" $(seq 1 $HORIZONTAL_LINE_WIDTH)
+    printf "\n"
+}
+
 software_info ()
 {
     uboot_version=$(tr -d '\0' </proc/device-tree/chosen/u-boot,version)
@@ -22,20 +61,18 @@ software_info ()
     fi
     hostname=$(cat /etc/hostname)
 
-    echo ""
-    echo "Software info"
-    echo "-----------------"
-    echo "U-Boot-version:[$uboot_version]"
-    echo "U-Boot vendor:[$uboot_env_vendor]"
-    echo "U-Boot video args:[$uboot_env_vidargs]"
-    echo "U-Boot secure boot:[$uboot_env_sec_boot]"
-    echo "U-Boot boot delay:[$uboot_env_bootdelay]"
-    echo "Kernel version:[$kernel_version]"
-    echo "Kernel command line:[$kernel_cmdline]"
-    echo "Distro name:[$distro_name]"
-    echo "Distro version:[$distro_version]"
-    echo "Hostname:[$hostname]"
-    echo "-----------------"
+    print_header "Software info"
+    print_info "U-Boot-version" "$uboot_version"
+    print_info "U-Boot vendor" "$uboot_env_vendor"
+    print_info "U-Boot video args" "$uboot_env_vidargs"
+    print_info "U-Boot secure boot" "$uboot_env_sec_boot"
+    print_info "U-Boot boot delay" "$uboot_env_bootdelay"
+    print_info "Kernel version" "$kernel_version"
+    print_info "Kernel command line" "$kernel_cmdline"
+    print_info "Distro name" "$distro_name"
+    print_info "Distro version" "$distro_version"
+    print_info "Hostname" "$hostname"
+    print_footer
 }
 
 hardware_info ()
@@ -49,17 +86,15 @@ hardware_info ()
     uboot_env_fdt_board=$(fw_printenv fdt_board | sed -r "s/.*=//g")
     uboot_env_soc=$(fw_printenv soc | sed -r "s/.*=//g")
 
-    echo ""
-    echo "Hardware info"
-    echo "-----------------"
-    echo "SoM model:[$som_model]"
-    echo "SoM version:[$som_pid4 $som_pid8]"
-    echo "SoM serial number:[$serial]"
-    echo "Processor arch:[$processor]"
-    echo "U-Boot board:[$uboot_env_board]"
-    echo "U-Boot fdt_board:[$uboot_env_fdt_board]"
-    echo "U-Boot soc:[$uboot_env_soc]"
-    echo "-----------------"
+    print_header "Hardware info"
+    print_info "SoM model" "$som_model"
+    print_info "SoM version" "$som_pid4 $som_pid8"
+    print_info "SoM serial number" "$serial"
+    print_info "Processor arch" "$processor"
+    print_info "U-Boot board" "$uboot_env_board"
+    print_info "U-Boot fdt_board" "$uboot_env_fdt_board"
+    print_info "U-Boot soc" "$uboot_env_soc"
+    print_footer
 }
 
 device_tree_info()
@@ -89,42 +124,34 @@ device_tree_info()
         fi
     fi
 
-    echo ""
-    echo "Device tree"
-    echo "-----------------"
-    echo "Device tree enabled:[$dt_used]"
-    echo "Compatible string:[$dt_compatible]"
-    echo "Device trees available:[$dt_available]"
-    echo "-----------------"
+    print_header "Device tree"
+    print_info "Device tree enabled" "$dt_used"
+    print_info "Compatible string" "$dt_compatible"
+    print_info "Device trees available" "$dt_available"
+    print_footer
 
-    echo ""
-    echo "Device tree overlays"
-    echo "-----------------"
-    echo "Overlays enabled:[$dto_enabled]"
-    echo "Overlays available:[$dto_available]"
-    echo "-----------------"
+    print_header "Device tree overlays"
+    print_info "Overlays enabled" "$dto_enabled"
+    print_info "Overlays available" "$dto_available"
+    print_footer
 }
 
 devices_info ()
 {
     devices=$(ls -lh /dev)
 
-    echo ""
-    echo "Devices"
-    echo "-----------------"
-    echo "All devices available:[$devices]"
-    echo "-----------------"
+    print_header "Devices"
+    print_info "All devices available" "$devices"
+    print_footer
 }
 
 modules_info ()
 {
     lsmod=$(lsmod)
 
-    echo ""
-    echo "Kernel modules"
-    echo "-----------------"
-    echo "Kernel modules loaded:[$lsmod]"
-    echo "-----------------"
+    print_header "Kernel modules"
+    print_info "Kernel modules loaded" "$lsmod"
+    print_footer
 }
 
 dmesg_log ()
