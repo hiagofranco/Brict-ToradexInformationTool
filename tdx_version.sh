cd6895d4f0be356464d2097f9701f7dc03b52bd2
Leonardo Graboski Veiga <leogveiga@gmail.com>
2022-07-17 14:20:50 -0300

    bootloader: unify on dedicated section
    Unify the bootloader information, present in both the "hardware" and
    "software" sections, in a "bootloader" dedicated section.
    
    Conditionally print GRUB or U-Boot info.

--- tdx_version.sh
+++ tdx_version.sh
@@ -49,11 +49,6 @@ print_footer ()
 
 software_summary ()
 {
-    uboot_version=$(tr -d '\0' </proc/device-tree/chosen/u-boot,version)
-    uboot_env_vendor=$(fw_printenv vendor | sed -r "s/.*=//g")
-    uboot_env_vidargs=$(fw_printenv vidargs | sed -r "s/.*=//g")
-    uboot_env_sec_boot=$(fw_printenv sec_boot | sed -r "s/.*=//g")
-    uboot_env_bootdelay=$(fw_printenv bootdelay | sed -r "s/.*=//g")
     kernel_version=$(uname -rv)
     kernel_cmdline=$(cat /proc/cmdline)
     if [ -f /etc/os-release ]; then
@@ -66,11 +61,7 @@ software_summary ()
     hostname=$(cat /etc/hostname)
 
     print_header "Software summary"
-    print_info "U-Boot-version" "$uboot_version"
-    print_info "U-Boot vendor" "$uboot_env_vendor"
-    print_info "U-Boot video args" "$uboot_env_vidargs"
-    print_info "U-Boot secure boot" "$uboot_env_sec_boot"
-    print_info "U-Boot boot delay" "$uboot_env_bootdelay"
+    print_info "Bootloader" "$BOOTLOADER"
     print_info "Kernel version" "$kernel_version"
     print_info "Kernel command line" "$kernel_cmdline"
     print_info "Distro name" "$distro_name"
@@ -86,23 +77,54 @@ hardware_info ()
     som_pid8=$(tr -d '\0' </proc/device-tree/toradex,board-rev)
     serial=$(tr -d '\0' </proc/device-tree/serial-number)
     processor=$(uname -m)
-    uboot_env_board=$(fw_printenv board | sed -r "s/.*=//g")
-    uboot_env_fdt_board=$(fw_printenv fdt_board | sed -r "s/.*=//g")
-    uboot_env_soc=$(fw_printenv soc | sed -r "s/.*=//g")
 
     print_header "Hardware info"
     print_info "SoM model" "$som_model"
     print_info "SoM version" "$som_pid4 $som_pid8"
     print_info "SoM serial number" "$serial"
     print_info "Processor arch" "$processor"
-    print_info "U-Boot board" "$uboot_env_board"
-    print_info "U-Boot fdt_board" "$uboot_env_fdt_board"
-    print_info "U-Boot soc" "$uboot_env_soc"
     print_footer
 }
 
-device_tree_info()
+bootloader_info ()
 {
+    print_header "Bootloader info"
+    if [ "$BOOTLOADER" = "U-Boot" ]; then
+        uboot_version=$(tr -d '\0' </proc/device-tree/chosen/u-boot,version)
+        if [ "$(which fw_printenv)" ]; then
+            uboot_env_vendor=$(fw_printenv vendor | sed -r "s/.*=//g")
+            uboot_env_board=$(fw_printenv board | sed -r "s/.*=//g")
+            uboot_env_fdt_board=$(fw_printenv fdt_board | sed -r "s/.*=//g")
+            uboot_env_soc=$(fw_printenv soc | sed -r "s/.*=//g")
+            uboot_env_vidargs=$(fw_printenv vidargs | sed -r "s/.*=//g")
+            uboot_env_sec_boot=$(fw_printenv sec_boot | sed -r "s/.*=//g")
+            uboot_env_bootdelay=$(fw_printenv bootdelay | sed -r "s/.*=//g")
+        fi
+
+        print_info "U-Boot version" "$uboot_version"
+        print_info "U-Boot vendor" "$uboot_env_vendor"
+        print_info "U-Boot board" "$uboot_env_board"
+        print_info "U-Boot fdt_board" "$uboot_env_fdt_board"
+        print_info "U-Boot soc" "$uboot_env_soc"
+        print_info "U-Boot video args" "$uboot_env_vidargs"
+        print_info "U-Boot secure boot" "$uboot_env_sec_boot"
+        print_info "U-Boot boot delay" "$uboot_env_bootdelay"
+    elif [ "$BOOTLOADER" = "GRUB" ]; then
+        grub_version=$(grub-install --version | awk -F ' ' '{printf $NF}')
+
+        print_info "GRUB version" "$grub_version"
+    else
+        print_info "Unknown bootloader"
+    fi
+    print_footer
+}
+
+device_tree_info ()
+{
+    if [ ! "$USE_DEVICETREE" ]; then
+        return
+    fi
+
     dt_compatible=$(tr -d '\0' </proc/device-tree/compatible)
     dt_used=$(fw_printenv fdtfile | sed -r "s/.*=//g")
     if [ -d /boot/ostree ]; then
@@ -183,12 +205,23 @@ distro_detect ()
     fi
 }
 
+devicetree_detect ()
+{
+    # fw_utils are not present on L4T
+    # fw_utils presennce does not guarantee U-Boot is used
+    # GRUB seem to have some sort of support for device tree
+    if find /boot/ -name "*dtb" | grep -q . ; then
+        export USE_DEVICETREE=1
+    else
+        export USE_DEVICETREE=""
+    fi
+}
+
 bootloader_detect ()
 {
     if [ -f /boot/grub/grub.cfg ] || [ "$(command -v grub-install)" ]; then
         export BOOTLOADER="GRUB"
-    # fw_utils - thus fw_printenv - are not present on L4T
-    elif find /boot/ -name "*dtb" | grep -q . || [ "$(command -v fw_printenv)" ]; then
+    elif [ "$USE_DEVICETREE" ]; then
         export BOOTLOADER="U-Boot"
     # Don't care about other bootloaders for now, only GRUB and U-Boot
     else
@@ -201,6 +234,7 @@ help_info ()
     echo "Usage: tdx_version.sh [OPTION]"
     echo "List information about hardware and software from Toradex modules."
     echo ""
+    echo "--bootloader, -b   : Display bootloader related information (U-Boot and GRUB only)."
     echo "--devices, -d      : List all devices in /dev/."
     echo "--device-tree, -dt : Display device tree and overlays related information."
     echo "--dmesg, -dm       : Export the dmesg output in a txt file at ~."
@@ -215,6 +249,7 @@ help_info ()
 
 check_root_user
 distro_detect
+devicetree_detect
 bootloader_detect
 
 case $1 in
@@ -226,6 +261,9 @@ case $1 in
         ;;
     "--hardware" | "-w")
         hardware_info
+        ;;
+    "--bootloader" | "-b")
+        bootloader_info
         ;;
     "--device-tree" | "-dt")
         device_tree_info
@@ -246,6 +284,7 @@ case $1 in
     "-a" | "--all" | *)
         software_summary
         hardware_info
+        bootloader_info
         device_tree_info
         devices_info
         modules_info
